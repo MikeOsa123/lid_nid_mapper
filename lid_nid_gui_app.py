21c5821b3e9195f12a2bea8950839d927de8c117
MikeOsa123 <m.osamwonyi@gmail.com>
2020-07-16 16:05:01 +0100

    updated script to click function

--- lid_nid_gui_app.py
+++ lid_nid_gui_app.py
@@ -34,8 +34,54 @@ class LidNidMapperGUI:
         # create text box labels
         self.client_label = Label(master, text="Client Name")
         self.client_label.grid(row=0, column=0, sticky=W)
-        
+        self.dropdown_label = Label(master, text="Campaign Type")
+        self.dropdown_label.grid(row=1, column=0, sticky=W)      
+        self.campaign_id_label = Label(master, text="Campaign ID")
+        self.campaign_id_label.grid(row=2, column=0, sticky=W)
 
+        # create entry box
+        self.client_entry = Entry(master, width=50)
+        self.client_entry.grid(row=0, column=1, sticky=W+E)
+        self.client_entry.insert(0, "Please enter the Client name: ")
+
+        self.campaign_id_entry = Entry(master, width=50)
+        self.campaign_id_entry.grid(row=2, column=1, sticky=W+E)
+        self.campaign_id_entry.insert(0, "Please provide list of numbers separated by comma, e.g. 1,2,3: ")
+        
         # functions for gui app
         def donothing():
             return 
+
+        # clear entry box
+        def clear_textbox(event):
+            event.widget.delete(0, END)
+            return None
+
+        # create buttons
+        self.submit_btn = Button(master, text="Submit Query", width=10, command=clicked)
+        self.submit_btn.grid(row=3, column=0, padx=10, pady=5, sticky=N+S+E+W)
+        self.query_btn = Button(master, text="Run Query", width=10, command=lambda: run_query(function) )
+        self.query_btn.grid(row=4, column=0, padx=10, pady=5, sticky=N+S+E+W)
+
+        # event handlers
+        self.client_entry.bind("<Button-1>", clear_textbox)
+        self.campaign_id_entry.bind("<Button-1>", clear_textbox)
+
+        # Button Functions
+        #-------------------------------------------------------------------------------------------------  
+
+        def clicked():
+
+            # disable submit button upon being clicked
+            self.submit_btn["state"] = DISABLED
+
+            # set query variables
+            self.client_name = self.client_entry.get().lower()
+            self.campaign_id = tuple(map(str, self.campaign_id_entry.get().split(',')))
+            self.campaign_type = self.variable.get()
+
+            # enable run query button
+            self.query_btn["state"] = NORMAL
+
+        def run_query(function):
+            return
